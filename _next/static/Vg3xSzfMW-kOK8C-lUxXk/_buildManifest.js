e62d54143248fa6f8a0cad12d25794ae3585b611
samuraitruong <samuraitruong@hotmail.com>
2023-04-12 11:19:05 +0000

    Deploying to gh-pages from @ samuraitruong/chess.com-daily-puzzle@573bb5c689513a30d536c5ad6e73a70ba69233d8 🚀

---
+++ _next/static/Vg3xSzfMW-kOK8C-lUxXk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/271-3adef7e2137f34f3.js","static/css/0224634fcf376fd6.css","static/chunks/pages/index-6c4617ac7a535fbc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
